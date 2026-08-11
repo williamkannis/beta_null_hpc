1b52e50c55242f8eb60359afeeb4c003ea75b15a
William Annis <wannis@fsu.edu>
2026-08-11 14:44:07 -0400

    added plotting code

--- Scripts/02_ses_comp.R
+++ Scripts/02_ses_comp.R
@@ -10,7 +10,8 @@
 
 # Description: Compiles and formats the resulting SES, ES, and diagnostic 
 # stats across files. Also visualizes normality diagnostics to allow users to 
-# decide between SES or ES values for further analyses
+# decide between SES or ES values for further analyses. Also include code for
+# summary plots
 
 
 # House Keeping ----------------------------------------------------------------
@@ -196,4 +197,179 @@ final_list <-lapply(analysis_list, function (x) {
 saveRDS(final_list$alpha,file.path(export_dir,"native_alpha.rds"))
 saveRDS(final_list$his_lcbd,file.path(export_dir,"native_lcbd.rds"))
 saveRDS(final_list$`_d_lcbd`,file.path(export_dir,"delta_lcbd.rds"))
+
+
+# Plotting examples  -----------------------------------------------------------
+
+# Code below, allows users to create plots demostrating changes in observed,
+# beta diversity effect sizes
+
+# Beta barplot -----------------------------------------------------------------
+
+# Extract and summarize pairwise beta diversity
+mpw_list <-lapply(c("mod_B","his_B"), function(i){
+  time_name = ifelse(i=="mod_B","Contemporary","Native")
+  out_list_t$obs[[i]] %>% 
+    summarise(across(everything(),~mean(.x))) %>% 
+    mutate(time = time_name) %>% 
+    tidyr::pivot_longer(-time)
+})
+
+# Combine and format for plotting
+mpw_df <- bind_rows(mpw_list) %>% 
+  mutate(
+    dim = sub("_mod","",name),
+    dim = sub("_his","",dim),
+    facet = substr(name,1,3),
+    component = sub(".*B", "", name),
+    facet =factor(facet,c("tax","fun","phy")),
+    time = factor(time, c("Native", "Contemporary"))
+  ) %>% 
+  filter(component != "total")
+
+# Plot
+pair <- ggplot(mpw_df, aes(x = time, y = value, fill = dim)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.7) +
+  facet_wrap(~facet)+
+  theme_classic(base_size = 25)+
+  ylab("")+
+  theme(legend.position = "none",
+        axis.title.x=element_blank())+
+  scale_fill_manual(values = c("#BF4354","#CCA6A7",
+                               "#4A7B97","#A6B0BD",
+                               "#CEA23F","#DCD0A7"))
+
+# Export
+ggsave(
+  filename = file.path(fig_dir,"mean_pairwise_beta.png"),
+  plot = pair,
+  width = 10,
+  height = 6,
+  dpi = 600
+)
+
+
+# Observed plots ---------------------------------------------------------------
+plot_metrics <-  c("_d_lcbd","_d_B")
+lapply(plot_metrics,function(i){
+  # Subset data
+  obs_plot_df <- out_list_t$obs[[i]] %>% 
+    tibble::rownames_to_column("COMID") %>% 
+    tidyr::pivot_longer(-COMID) %>% 
+    mutate(name = sub("_d","",name),
+           name = sub("rf","",name))
+  
+  # Set factor levels
+  plot_factor <- c("tax_Btotal","tax_Brepl","tax_Brich",
+                   "fun_Btotal","fun_Brepl","fun_Brich",
+                   "phy_Btotal","phy_Brepl","phy_Brich")
+  obs_plot_df$name <- factor(obs_plot_df$name,plot_factor)
+  
+  # Plot
+  obs_vio <- ggplot(
+    obs_plot_df, 
+    aes(x = name, y = value, fill = name,color = name)
+    ) +
+    geom_boxplot()+
+    stat_summary(
+      geom = "crossbar", 
+      width = 0.75,         
+      fatten = 2,         
+      color = "black",        
+      fun.data = function(x){ 
+        return(c(y = median(x), ymin = median(x), ymax = median(x))) 
+                 }
+      )+
+    geom_hline(yintercept=0, linetype="dashed", color = "red",size = 1)+
+    theme_classic(base_size = 25)+
+    theme(
+      legend.position = "none",
+       panel.border =  element_rect(color = "black", fill = NA, size = 2.5),
+       axis.title.y=element_blank(),
+       axis.title.x=element_blank(),
+       axis.text.x=element_blank()) +
+    scale_y_continuous(breaks = pretty) +
+    scale_fill_manual(values = c("#CEA23F","#CEA23F","#CEA23F",
+                                 "#BF4354","#BF4354","#BF4354",
+                                 "#4A7B97","#4A7B97","#4A7B97"))+
+    scale_color_manual(values = c("#CEA23F","#CEA23F","#CEA23F",
+                                  "#BF4354","#BF4354","#BF4354",
+                                  "#4A7B97","#4A7B97","#4A7B97"))
+  
+  # Export
+  obs_name <- paste0("obs_",sub("_d_","",i),"_box.png")
+  ggsave(
+    filename = file.path(fig_dir,obs_name),
+    plot = obs_vio,
+    width = 10,
+    height = 6,
+    dpi = 600
+  )
+})
+
+
+# Effect size plots  -----------------------------------------------------------
+
+plot_metrics <-  c("_d_lcbd","_d_B")
+lapply(plot_metrics,function(i){
+  # Subset and format data
+  es_plot_df <- out_list_t$empirical_es[[i]] %>% 
+    tibble::rownames_to_column("COMID") %>% 
+    tidyr::pivot_longer(-COMID) %>% 
+    mutate(name = sub("_d","",name),
+           name = sub("rc","",name))
+  
+  # Set factor levels
+  plot_factor <- c("tax_Btotal","tax_Brepl","tax_Brich",
+                   "fun_Btotal","fun_Brepl","fun_Brich",
+                   "phy_Btotal","phy_Brepl","phy_Brich")
+  es_plot_df$name <- factor(es_plot_df$name,plot_factor)
+  
+  # Remove taxonomic
+  # es_plot_df <- es_plot_df %>% 
+  #   filter(substr(name,1,1) != "t")
+  
+  # Plot
+  es_vio <- ggplot(
+    es_plot_df, 
+    aes(x = name, y = value, fill = name,color = name)
+    ) +
+    geom_boxplot()+
+    stat_summary(
+     geom = "crossbar", 
+     width = 0.75,         
+     fatten = 2,         
+     color = "black",        
+     fun.data = function(x){ 
+       return(c(y = median(x), ymin = median(x), ymax = median(x))) 
+     })+
+    geom_hline(yintercept=0, linetype="dashed", color = "red",size = 1)+
+    geom_hline(yintercept=1.96, linetype="dashed", color = "black",size = 1)+
+    geom_hline(yintercept=-1.96, linetype="dashed", color = "black",size = 1)+
+    theme_classic(base_size = 25)+
+    theme(legend.position = "none",
+          panel.border =  element_rect(color = "black", fill = NA, size = 2.5),
+          axis.title.y=element_blank(),
+          axis.title.x=element_blank(),
+          axis.text.x=element_blank()) +
+    scale_y_continuous(breaks = pretty) +
+    scale_fill_manual(values = c(
+      "#CEA23F","#CEA23F","#CEA23F",
+      "#BF4354","#BF4354","#BF4354",
+      "#4A7B97","#4A7B97","#4A7B97"))+
+    scale_color_manual(values = c(
+      "#CEA23F","#CEA23F","#CEA23F",
+      "#BF4354","#BF4354","#BF4354",
+      "#4A7B97","#4A7B97","#4A7B97"))
+  
+  #Export
+  es_name <- paste0("es_",sub("_d_","",i),"_box_nosig.png")
+  ggsave(
+    filename = file.path(fig_dir,es_name),
+    plot = es_vio,
+    width = 10,
+    height = 6,
+    dpi = 600
+  )
+})
 
