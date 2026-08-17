84944c5ee5e2b228443923005a3ddfda4e0d3062
William Annis <wannis@fsu.edu>
2026-08-16 21:16:34 -0400

    add code to prepare ses inputs, and batch estimate within the same node

--- dev/14_ses_batch.R
+++ dev/14_ses_batch.R
@@ -11,33 +11,69 @@
 # Description: Calculated standardized effects sizes, empirical p values, and
 # diagnostic stats for a diversity metric defined by a shell script argument
 
+## SET SHELL SCRIPT TO HAVE NUMBER OF COMPONENTD PLUS 1 IF LCBD = T
+
 # House keeping  ---------------------------------------------------------------
 rm(list = ls())
 
 # Packages
+# LOAD IN CUSTOM PACKAGE
 library(dplyr)
+library(purr)
 
 # Directories
-input_dir <- "HPC/ses_inputs"
-out_dir <- "HPC/ses_outputs"
+obs_dir <- file.path(dir,"obs_out")
+null_dir <- file.path(dir,"null_out")
+out_dir <- file.path(dir,"ses_outputs")
+dir.create(out_dir,recursive = T)
 
-# Load in custom SES function
-source("HPC/scripts_do_not_run/null_model_effect_size_function.R")
 
-# Load in data based on shell script argument
-arg <- as.numeric(commandArgs(trailingOnly = TRUE))  # shell script argument
-input_file <- list.files(input_dir)[arg]
-input <- readRDS(file.path(input_dir,input_file))
+# Load in obs and null data ----------------------------------------------------
+
+obs <- readRDS(list.files(obs_dir))
+
+# Combine null processing chunks into one list
+null_files <- list.files(null_dir)
+null_list <- lapply(
+  null_files, 
+  function(x) readRDS(file.path(null_dir,x))
+)
+null <- unlist(null_list,recursive = FALSE)
+
+
+obs_list <- list(Beta = list(Btotal = 1,Brepl = 1),lcbd = 1)
+null_list <- list(obs_list,obs_list,obs_list)
+
+obs_input <- unlist(obs_list,recursive = FALSE)
+
+null_list_t <- purrr::transpose(null_list)
+null_input <- purrr::transpose(null_list_t[[1]])
+## FIX DIV FUNCTION TO MAKE A CLEAN LIST ITH NO NESRING
+if("lcbd" %in% names(null_list_t)) {
+  null_input$lcbd <- null_list_t[[2]]
+}
+
 
 # Run SES function and export  -------------------------------------------------
-out <- ses_fun(
-  obs =input$obs,
-  null_list =input$null_list,
-  diag = T, 
-  emp_padding =1
-  )
 
-# Export
+out_list <- parallel::mclapply(1:length(obs_input), function(i){
+  
+  obs <- obs_input[[i]]
+  null_list <- null_input[[i]]
+  
+  ses_fun(
+    obs = obs,
+    null_list = null_list,
+    diag = T, 
+    emp_padding =1
+  )
+}
+)
+names(out_list) <- names(obs_input)
+
+
+
+# Export -----------------------------------------------------------------------
 out_name <-sub("ses_input","ses_out",input_file)
 saveRDS(out,file.path(out_dir,out_name))
 
